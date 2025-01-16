91272846b62efd99a9df3206ef9db02afa8ef06c
xyzelena <emkhuzhina@gmail.com>
2025-01-16 18:22:40 +0530

    Test Husky

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,8 +1,6 @@
 import { TrpcProvider } from './lib/trpc';
 import { AllIdeasPage } from './pages/AllIdeasPage/index';
 
-const x: number = 'csc';
-
 const App = () => {
   return (
     <TrpcProvider>
