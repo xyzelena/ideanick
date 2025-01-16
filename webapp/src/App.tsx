391695f2e872a75aa8faadfbfb9969fb0ff421d8
xyzelena <emkhuzhina@gmail.com>
2025-01-16 11:47:54 +0530

    fix with prettify and add check types

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,12 +1,12 @@
-import { TrpcProvider } from './lib/trpc'
-import { AllIdeasPage } from './pages/AllIdeasPage/index'
+import { TrpcProvider } from './lib/trpc';
+import { AllIdeasPage } from './pages/AllIdeasPage/index';
 
 const App = () => {
   return (
     <TrpcProvider>
       <AllIdeasPage />
     </TrpcProvider>
-  )
-}
+  );
+};
 
 export default App;
--- webapp/src/lib/trpc.tsx
+++ webapp/src/lib/trpc.tsx
@@ -1,31 +1,31 @@
-import type { TrpcRouter } from '@ideanick/backend/src/trpc'
-import { createTRPCReact } from '@trpc/react-query'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { httpBatchLink } from '@trpc/client'
+import type { TrpcRouter } from '@ideanick/backend/src/trpc';
+import { createTRPCReact } from '@trpc/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { httpBatchLink } from '@trpc/client';
 
-export const trpc = createTRPCReact<TrpcRouter>()
+export const trpc = createTRPCReact<TrpcRouter>();
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            retry: false,
-            refetchOnWindowFocus: false,
-        },
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
     },
-})
+  },
+});
 
 const trpcClient = trpc.createClient({
-    links: [
-        httpBatchLink({
-            url: 'http://localhost:3000/trpc',
-        }),
-    ],
-})
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:3000/trpc',
+    }),
+  ],
+});
 
 export const TrpcProvider = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <trpc.Provider client={trpcClient} queryClient={queryClient}>
-            <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-        </trpc.Provider>
-    )
-}
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
+};
