542d2b5ef664a7e9d38d3c80d87538a860060344
xyzelena <emkhuzhina@gmail.com>
2025-01-16 18:15:44 +0530

    install husky

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,6 +1,8 @@
 import { TrpcProvider } from './lib/trpc';
 import { AllIdeasPage } from './pages/AllIdeasPage/index';
 
+const x: number = 'csc';
+
 const App = () => {
   return (
     <TrpcProvider>
