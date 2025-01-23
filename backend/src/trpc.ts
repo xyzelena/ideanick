d829ae00565ff6664272001eaa3db5f7bb4f8cf1
xyzelena <emkhuzhina@gmail.com>
2025-01-23 15:11:35 +0530

    With Zod, creating a tRPC procedure with input parameters

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,5 +1,6 @@
 import { initTRPC } from '@trpc/server';
 import _ from 'lodash';
+import { z } from 'zod';
 
 const ideas = _.times(100, (i) => ({
   nick: `cool-idea-nick-${i}`,
@@ -14,6 +15,16 @@ export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
     return { ideas: ideas.map((idea) => _.pick(idea, ['nick', 'name', 'description'])) };
   }),
+  getIdea: trpc.procedure
+    .input(
+      z.object({
+        ideaNick: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      const idea = ideas.find((idea) => idea.nick === input.ideaNick);
+      return { idea: idea || null };
+    }),
 });
 
 export type TrpcRouter = typeof trpcRouter;
--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -1,18 +1,31 @@
 import { useParams } from 'react-router-dom';
 import { type ViewIdeaRouteParams } from '../../lib/routes';
+import { trpc } from '../../lib/trpc';
 
 export const ViewIdeaPage = () => {
   const { ideaNick } = useParams() as ViewIdeaRouteParams;
 
+  const { data, error, isLoading, isFetching, isError } = trpc.getIdea.useQuery({
+    ideaNick,
+  });
+
+  if (isLoading || isFetching) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  if (!data.idea) {
+    return <span>Idea not found</span>;
+  }
+
   return (
     <div>
-      <h1>{ideaNick}</h1>
-      <p>Description of idea 1...</p>
-      <div>
-        <p>Text paragrph 1 of idea 1...</p>
-        <p>Text paragrph 2 of idea 1...</p>
-        <p>Text paragrph 3 of idea 1...</p>
-      </div>
+      <h1>{data.idea.name}</h1>
+      <p>{data.idea.description}</p>
+      <div dangerouslySetInnerHTML={{ __html: data.idea.text }} />
     </div>
   );
 };
