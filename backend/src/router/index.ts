433f948a6c08d92ea43ffc47b8c0997603e93d83
xyzelena <emkhuzhina@gmail.com>
2025-02-07 00:56:55 +0530

    Add rules for Generate Index extension

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,10 +1,14 @@
 import { trpc } from '../lib/trpc';
+// @index('./**/index.ts', f => `import { ${f.path.split('/').slice(0, -1).pop()}TrpcRoute } from '${f.path.split('/').slice(0, -1).join('/')}'`)
 import { getIdeaTrpcRoute } from './getIdea';
 import { getIdeasTrpcRoute } from './getIdeas';
+// @endindex
 
 export const trpcRouter = trpc.router({
+  // @index('./**/index.ts', f => `${f.path.split('/').slice(0, -1).pop()}: ${f.path.split('/').slice(0, -1).pop()}TrpcRoute,`)
   getIdea: getIdeaTrpcRoute,
   getIdeas: getIdeasTrpcRoute,
+  // @endindex
 });
 
 export type TrpcRouter = typeof trpcRouter;
