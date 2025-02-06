b18ac67c26bd06b3b3d2264086e28f7dd4fc6b0f
xyzelena <emkhuzhina@gmail.com>
2025-02-06 17:07:45 +0530

    Add components Textarea and Input

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,5 +1,40 @@
+import { useState } from 'react';
+
+import { Input } from '../../components/Input';
 import { Segment } from '../../components/Segment';
+import { Textarea } from '../../components/Textarea';
 
 export const NewIdeaPage = () => {
-  return <Segment title="New Idea">Form will be here...</Segment>;
+  const [state, setState] = useState({
+    name: '',
+    nick: '',
+    description: '',
+    text: '',
+  });
+
+  return (
+    <Segment title="New Idea">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.info('Submitted', state);
+        }}
+      >
+        <Input name="name" label="Name" state={state} setState={setState} />
+
+        <Input name="nick" label="Nick" state={state} setState={setState} />
+
+        <Input
+          name="description"
+          label="Description"
+          state={state}
+          setState={setState}
+        />
+
+        <Textarea name="text" label="Text" state={state} setState={setState} />
+
+        <button type="submit">Create Idea</button>
+      </form>
+    </Segment>
+  );
 };
