d54ffb14256eb329369bad407e3505549fcec51d
xyzelena <emkhuzhina@gmail.com>
2025-03-18 12:52:51 +0530

    Add FormItems

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 
 import { Alert } from '../../components/Alert';
 import { Button } from '../../components/Button';
+import { FormItems } from '../../components/FormItems';
 import { Input } from '../../components/Input';
 import { Segment } from '../../components/Segment';
 import { Textarea } from '../../components/Textarea';
@@ -51,28 +52,26 @@ export const NewIdeaPage = () => {
           formik.handleSubmit();
         }}
       >
-        <Input name="name" label="Name" formik={formik} />
+        <FormItems>
+          <Input name="name" label="Name" formik={formik} />
+          <Input name="nick" label="Nick" formik={formik} />
+          <Input
+            name="description"
+            label="Description"
+            formik={formik}
+            maxWidth={500}
+          />
+          <Textarea name="text" label="Text" formik={formik} />
+          {!formik.isValid && !!formik.submitCount && (
+            <div style={{ color: 'red' }}>Some fields are invalid</div>
+          )}
 
-        <Input name="nick" label="Nick" formik={formik} />
+          {!!submittingError && <Alert color="red">{submittingError}</Alert>}
 
-        <Input
-          name="description"
-          label="Description"
-          formik={formik}
-          maxWidth={500}
-        />
+          {successMessageVisible && <Alert color="green">Idea created!</Alert>}
 
-        <Textarea name="text" label="Text" formik={formik} />
-
-        {!formik.isValid && !!formik.submitCount && (
-          <div style={{ color: 'red' }}>Some fields are invalid</div>
-        )}
-
-        {!!submittingError && <Alert color="red">{submittingError}</Alert>}
-
-        {successMessageVisible && <Alert color="green">Idea created!</Alert>}
-
-        <Button loading={formik.isSubmitting}>Create Idea</Button>
+          <Button loading={formik.isSubmitting}>Create Idea</Button>
+        </FormItems>
       </form>
     </Segment>
   );
