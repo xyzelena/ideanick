276447186bf9d26b3ff5a001cc443e5e45ae4679
xyzelena <emkhuzhina@gmail.com>
2025-06-23 01:32:56 +0530

    Improved page wrapper typing

--- webapp/src/pages/EditIdeaPage/index.tsx
+++ webapp/src/pages/EditIdeaPage/index.tsx
@@ -14,15 +14,16 @@ export const EditIdeaPage = withPageWrapper({
       ideaNick,
     });
   },
-  checkExists: ({ queryResult }) => !!queryResult.data.idea,
-  checkExistsMessage: 'Idea not found',
-  checkAccess: ({ queryResult, ctx }) =>
-    !!ctx.me && ctx.me.id === queryResult.data.idea?.authorId,
-  checkAccessMessage: 'An idea can only be edited by the author',
-  setProps: ({ queryResult }) => ({
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    idea: queryResult.data.idea!,
-  }),
+  setProps: ({ queryResult, ctx, checkExists, checkAccess }) => {
+    const idea = checkExists(queryResult.data.idea, 'Idea not found');
+    checkAccess(
+      ctx.me?.id === idea.authorId,
+      'An idea can only be edited by the author'
+    );
+    return {
+      idea,
+    };
+  },
 })(({ idea }) => {
   return <EditIdeaComponent idea={idea} />;
 });
--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -17,11 +17,8 @@ export const ViewIdeaPage = withPageWrapper({
       ideaNick,
     });
   },
-  checkExists: ({ queryResult }) => !!queryResult.data.idea,
-  checkExistsMessage: 'Idea not found',
-  setProps: ({ queryResult, ctx }) => ({
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    idea: queryResult.data.idea!,
+  setProps: ({ queryResult, checkExists, ctx }) => ({
+    idea: checkExists(queryResult.data.idea, 'Idea not found'),
     me: ctx.me,
   }),
 })(({ idea, me }) => (
